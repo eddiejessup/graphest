a6f02d020897f2d0cb7b85633284829e90f11475
Elliot Marsden <elliot.marsden@gmail.com>
2017-04-03 22:28:24 +0200

    Add acceleration

--- graphest.go
+++ graphest.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"fmt"
+	"errors"
+	"log"
 	"github.com/eddiejessup/vec"
 	"github.com/fogleman/gg"
+	"github.com/golang/geo/r2"
 	"math"
+	"math/rand"
+	"image/color"
+	"bytes"
+	"encoding/binary"
+	"hash/crc32"
 )
 
 type Body struct {
@@ -14,7 +22,7 @@ type Body struct {
 
 type Mover struct {
 	Body
-	Velocity  vec.Vector
+	Velocity  r2.Point
 	TimeToLay int
 }
 
@@ -23,14 +31,19 @@ type Egg struct {
 	TimeToHatch int
 }
 
-type RGB struct {
-	R, G, B float64
-}
-
-var rgbs = []RGB{
-	{0, 0, 0},
-	{0, 100, 0},
-	{70, 10, 30},
+var palette = color.Palette{
+	color.RGBA{141, 211, 199, 0},
+	color.RGBA{255, 255, 179, 0},
+	color.RGBA{190, 186, 218, 0},
+	color.RGBA{251, 128, 114, 0},
+	color.RGBA{128, 177, 211, 0},
+	color.RGBA{253, 180, 98, 0},
+	color.RGBA{179, 222, 105, 0},
+	color.RGBA{252, 205, 229, 0},
+	color.RGBA{217, 217, 217, 0},
+	color.RGBA{188, 128, 189, 0},
+	color.RGBA{204, 235, 197, 0},
+	color.RGBA{255, 237, 111, 0},
 }
 
 const (
@@ -40,14 +53,23 @@ const (
 	TimeMax          = 100
 	IncubationPeriod = 10
 	LayingPeriod     = 10
+	V = 10
+	SmallNumber = 1e-3
+	Acceleration = 1.0
 )
 
 func drawWrappedCircle(dc *gg.Context, x, y, r float64) {
 	dc.DrawCircle(x, y, r)
-	dc.DrawCircle(x+X, y, r)
-	dc.DrawCircle(x-X, y, r)
-	dc.DrawCircle(x, y+Y, r)
 	dc.DrawCircle(x, y-Y, r)
+	dc.DrawCircle(x, y+Y, r)
+
+	dc.DrawCircle(x-X, y, r)
+	dc.DrawCircle(x-X, y-Y, r)
+	dc.DrawCircle(x-X, y+Y, r)
+
+	dc.DrawCircle(x+X, y, r)
+	dc.DrawCircle(x+X, y-Y, r)
+	dc.DrawCircle(x+X, y+Y, r)
 }
 
 func drawWrappedCircleStruct(dc *gg.Context, c vec.Circle) {
@@ -56,7 +78,7 @@ func drawWrappedCircleStruct(dc *gg.Context, c vec.Circle) {
 
 func getLimbCircle(core vec.Circle, ang float64) vec.Circle {
 	return vec.Circle{
-		Centre: vec.Vector{
+		Centre: r2.Point{
 			X: core.Centre.X + 2*core.R*math.Cos(ang),
 			Y: core.Centre.Y + 2*core.R*math.Sin(ang),
 		},
@@ -78,18 +100,23 @@ func drawWrappedBody(dc *gg.Context, b Body) {
 	}
 }
 
-func setRGB(dc *gg.Context, rgb RGB) {
-	dc.SetRGB(rgb.R, rgb.G, rgb.B)
+func setRGB(dc *gg.Context, color color.Color) {
+	r, g, b, _ := color.RGBA()
+	dc.SetRGB(float64(r), float64(g), float64(b))
 }
 
 func moveMover(m *Mover) {
-	m.Body.Core.Centre.X += m.Velocity.X
-	m.Body.Core.Centre.Y += m.Velocity.Y
+	m.Body.Core.Centre = m.Body.Core.Centre.Add(m.Velocity)
+	// Wrap.
 	if m.Body.Core.Centre.X > X {
 		m.Body.Core.Centre.X -= X
+	} else if m.Body.Core.Centre.X < 0 {
+		m.Body.Core.Centre.X += X
 	}
 	if m.Body.Core.Centre.Y > Y {
 		m.Body.Core.Centre.Y -= Y
+	} else if m.Body.Core.Centre.Y < 0 {
+		m.Body.Core.Centre.Y += Y
 	}
 }
 
@@ -98,13 +125,27 @@ func bodyArea(b Body) float64 {
 }
 
 func moverMomentum(m Mover) float64 {
-	return vec.VectorMag(m.Velocity) * bodyArea(m.Body)
+	return m.Velocity.Norm() * bodyArea(m.Body)
+}
+
+
+func WrappedCirclesIntersect(c1, c2 vec.Circle) bool {
+	return (
+		vec.CirclesIntersect(c1, c2.Offset(r2.Point{-X, -Y})) ||
+		vec.CirclesIntersect(c1, c2.Offset(r2.Point{-X, 0})) ||
+		vec.CirclesIntersect(c1, c2.Offset(r2.Point{-X, +Y})) ||
+		vec.CirclesIntersect(c1, c2.Offset(r2.Point{0, -Y})) ||
+		vec.CirclesIntersect(c1, c2.Offset(r2.Point{0, 0})) ||
+		vec.CirclesIntersect(c1, c2.Offset(r2.Point{0, +Y})) ||
+		vec.CirclesIntersect(c1, c2.Offset(r2.Point{+X, -Y})) ||
+		vec.CirclesIntersect(c1, c2.Offset(r2.Point{+X, 0})) ||
+		vec.CirclesIntersect(c1, c2.Offset(r2.Point{+X, +Y})))
 }
 
 func bodiesIntersect(b1, b2 Body) bool {
 	for _, c1 := range bodyCircles(b1) {
 		for _, c2 := range bodyCircles(b2) {
-			if vec.CirclesIntersect(c1, c2) {
+			if WrappedCirclesIntersect(c1, c2) {
 				return true
 			}
 		}
@@ -112,24 +153,42 @@ func bodiesIntersect(b1, b2 Body) bool {
 	return false
 }
 
-func update(movers *[]Mover, eggs *[]Egg) {
+func update(movers *[]Mover, eggs *[]Egg) error {
 	// Move the movers.
 	for i := range *movers {
 		moveMover(&((*movers)[i]))
 	}
 
 	// Find the killed.
-	var moverIndsToDelete []int
+	killedSet := make(map[*Mover]bool)
 	for i := range *movers {
 		for j := range *movers {
-			if (i != j) && bodiesIntersect((*movers)[i].Body, (*movers)[j].Body) && (moverMomentum((*movers)[i]) > moverMomentum((*movers)[j])) {
-				moverIndsToDelete = append(moverIndsToDelete, j)
+			if (i != j) {
+				iMom := moverMomentum((*movers)[i])
+				jMom := moverMomentum((*movers)[j])
+				if iMom == jMom {
+					return errors.New(fmt.Sprintf("Found two movers with equal momenta: %v and %v", iMom, jMom))
+				}
+				if bodiesIntersect((*movers)[i].Body, (*movers)[j].Body) && (iMom > jMom) {
+					killedSet[&(*movers)[j]] = true
+				}
 			}
 		}
 	}
 	// Kill the killed.
-	for _, v := range moverIndsToDelete {
-		*movers = append((*movers)[:v], (*movers)[v+1:]...)
+	var stillMovers []Mover
+	for i := range *movers {
+		mover := &(*movers)[i]
+		if !killedSet[mover] {
+			stillMovers = append(stillMovers, *mover)
+		}
+	}
+	*movers = stillMovers
+
+	// Accelerate the movers.
+	for i := range *movers {
+		mover := &(*movers)[i]
+		mover.Velocity = mover.Velocity.Add(mover.Velocity.Normalize().Mul(Acceleration))
 	}
 
 	// Lay the movers' prepared eggs.
@@ -149,7 +208,7 @@ func update(movers *[]Mover, eggs *[]Egg) {
 	}
 
 	// Hatch the incubated eggs.
-	var eggIndsToDelete []int
+	hatchedSet := make(map[*Egg]bool)
 	for i := range *eggs {
 		egg := &(*eggs)[i]
 		if egg.TimeToHatch > 0 {
@@ -165,17 +224,25 @@ func update(movers *[]Mover, eggs *[]Egg) {
 			if !eggIntersectsAMover {
 				newMover := Mover{
 					Body:      egg.Body,
+					Velocity: initialVelocity(),
 					TimeToLay: IncubationPeriod,
 				}
 				*movers = append(*movers, newMover)
-				eggIndsToDelete = append(eggIndsToDelete, i)
+				hatchedSet[egg] = true
 			}
 		}
 	}
+
 	// Clean up the born's eggs.
-	for _, v := range eggIndsToDelete {
-		*eggs = append((*eggs)[:v], (*eggs)[v+1:]...)
+	var stillEggs []Egg
+	for i := range *eggs {
+		egg := &(*eggs)[i]
+		if !hatchedSet[egg] {
+			stillEggs = append(stillEggs, *egg)
+		}
 	}
+	*eggs = stillEggs
+	return nil
 }
 
 func draw(dc *gg.Context, movers []Mover, eggs []Egg, t int) {
@@ -184,13 +251,15 @@ func draw(dc *gg.Context, movers []Mover, eggs []Egg, t int) {
 	dc.Clear()
 
 	// Draw the scene.
-	for i, mover := range movers {
-		setRGB(dc, rgbs[i])
+	for _, mover := range movers {
+		colorIndex := hashBody(mover.Body) % 10
+		setRGB(dc, palette[colorIndex])
 		drawWrappedBody(dc, mover.Body)
 		dc.Fill()
 	}
-	for i, egg := range eggs {
-		setRGB(dc, rgbs[i])
+	for _, egg := range eggs {
+		colorIndex := hashBody(egg.Body) % 10
+		setRGB(dc, palette[colorIndex])
 		drawWrappedBody(dc, egg.Body)
 		dc.Stroke()
 	}
@@ -200,22 +269,36 @@ func draw(dc *gg.Context, movers []Mover, eggs []Egg, t int) {
 	dc.SavePNG(fileName)
 }
 
+
+func hashBody(body Body) int {
+	var buf bytes.Buffer
+	binary.Write(&buf, binary.LittleEndian, body.Limbs)
+	return int(crc32.ChecksumIEEE(buf.Bytes()))
+
+}
+
+
+func initialVelocity() r2.Point {
+	return vec.RandomUnitVector().Mul(rand.Float64() * SmallNumber)
+}
+
+
 func main() {
 	movers := []Mover{
 		Mover{
 			Body: Body{
-				Core:  vec.Circle{Centre: vec.Vector{100, 100}, R: R},
+				Core:  vec.Circle{Centre: r2.Point{100, 100}, R: R},
 				Limbs: []float64{0.0, math.Pi / 2},
 			},
-			Velocity:  vec.Vector{X: 5, Y: 5},
+			Velocity:  initialVelocity(),
 			TimeToLay: LayingPeriod,
 		},
 		Mover{
 			Body: Body{
-				Core:  vec.Circle{Centre: vec.Vector{400, 400}, R: R},
+				Core:  vec.Circle{Centre: r2.Point{400, 400}, R: R},
 				Limbs: []float64{0.0, -math.Pi / 2},
 			},
-			Velocity:  vec.Vector{X: -5.1, Y: -5},
+			Velocity: initialVelocity(),
 			TimeToLay: LayingPeriod,
 		},
 	}
@@ -223,7 +306,7 @@ func main() {
 	eggs := []Egg{
 		Egg{
 			Body: Body{
-				Core:  vec.Circle{Centre: vec.Vector{100, 100}, R: R},
+				Core:  vec.Circle{Centre: r2.Point{100, 100}, R: R},
 				Limbs: []float64{0.0, math.Pi / 2},
 			},
 			TimeToHatch: IncubationPeriod,
@@ -232,7 +315,10 @@ func main() {
 
 	dc := gg.NewContext(X, Y)
 	for t := 0; t < TimeMax; t++ {
-		update(&movers, &eggs)
+		err := update(&movers, &eggs)
+		if err != nil {
+	        log.Fatal(err)
+		}
 		draw(dc, movers, eggs, t)
 	}
 }
